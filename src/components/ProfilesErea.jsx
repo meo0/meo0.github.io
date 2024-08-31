88fdd48e5fe0a012c32f9db5c0f0827e53b81b2c
meo0 <hashidamemos@gmail.com>
2024-08-31 16:25:09 +0900

    Add GitHub profile view

---
+++ src/components/ProfilesErea.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const GitHubUserProfile = ({ username }) => {
+  const [userData, setUserData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+          throw new Error('User not found');
+        }
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, [username]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      <Avatar src={userData.avatar_url} alt={`${username}'s avatar`} sx={{width:'100%', height:'100%'}} />
+      <h2>{userData.name || 'No name provided'}</h2>
+      <p>{userData.bio || 'No bio available'}</p>
+      {/*<p>Followers: {userData.followers}</p>
+      <p>Following: {userData.following}</p>
+      <p>Public Repos: {userData.public_repos}</p>*/}
+      <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+        <GitHubIcon/>
+      </a>
+    </div>
+  );
+};
+
+export const ProfilesErea = () => {
+    return (
+        <Box>
+            <GitHubUserProfile username={'meo0'}/>
+        </Box>
+    )
+}
